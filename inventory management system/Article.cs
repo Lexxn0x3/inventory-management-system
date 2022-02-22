23c533e4869a1768ffe162d536d674bb9f27ca7b
Robin Rosner <robin.rosner@siemens.com>
2022-02-22 18:08:12 +0100

    Class

---
+++ inventory management system/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventory_management_system
+{
+    internal class Article
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public double Prize { get; set; }
+        public string ArtNr { get; set; }
+
+        public Article(string name, int count, double prize, string ArtNr)
+        {
+            this.Name = name;
+            this.Count = count;
+            this.Prize = prize;
+            this.ArtNr = ArtNr;
+        }
+    }
+}
