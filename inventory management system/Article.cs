574baca4c6f7c7218f75eb203ecbb002b6f970a3
Robin Rosner <robin.rosner@siemens.com>
2022-02-22 20:22:34 +0100

    2

--- inventory management system/Article.cs
+++ inventory management system/Article.cs
@@ -8,17 +8,81 @@ namespace inventory_management_system
 {
     internal class Article
     {
+        private static int currentID;
+
+        protected int ID;
         public string Name { get; set; }
         public int Count { get; set; }
         public double Prize { get; set; }
-        public string ArtNr { get; set; }
 
-        public Article(string name, int count, double prize, string ArtNr)
+        public Article(string name, int count = 0, double prize = 0)
         {
             this.Name = name;
             this.Count = count;
             this.Prize = prize;
-            this.ArtNr = ArtNr;
+            this.ID = GetNextID();
+        }
+
+        static Article() => currentID = 0;
+        protected int GetNextID() => ++currentID;
+
+        public override string ToString()
+        {
+            string str = "";
+
+            str = $"{ID:d5}, {Name}, x{Count}, {Prize}$";
+            return str;
+        }
+
+        //internal static Article getArticle(int sellArtNr, List<Article> list)
+        //{
+        //    for (int i = 0; i < list.Count; i++)
+        //    {
+        //        if (list[i].ID == sellArtNr)
+        //            return list[i];
+        //    }           
+        //}
+
+        private static bool FindArtWithArtNr(Article article, int sellArtNr)
+        {
+            return article.ID == sellArtNr;
+        }
+
+        internal static object getPrize(int sellCount, int sellArtNr, List<Article> list)
+        {
+            return list[sellArtNr-1].Prize*sellCount;
+        }
+
+        internal static bool Exists(int sellArtNr, List<Article> list)
+        {
+            return list.Count >= sellArtNr;
+        }
+
+        internal static bool CountAvailable(int sellArtNr, int sellCount, List<Article> lager)
+        {
+            return lager[sellArtNr-1].Count > sellCount;
+        }
+    }
+
+    class Electronic : Article
+    {
+        public int Power { get; set; }
+
+        public Electronic(string name, int count = 0, double prize = 0, int power = 0) : base(name, count, prize)
+        {
+            this.Name=name;
+            this.Count=count;
+            this.Power = power;
+            this.Prize=prize;
+            this.ID = GetNextID()-1; //-1 cs 2x constructor
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+
+            str = $"{ID:d5}, {Name}, x{Count}, {Prize}$, {Power}Watt";
+            return str;
         }
     }
 }
--- inventory management system/Program.cs
+++ inventory management system/Program.cs
@@ -1,1 +1,168 @@
-﻿
+﻿using System;
+
+namespace inventory_management_system
+{
+    class inventory_management_system
+    {
+        private static List<Article> Lager = new List<Article>();
+        private new const String Abstandhalter = "\n--------------------------------------------------------------\n";
+
+        static void Main(string[] args)
+        {
+            //Article List
+
+            Lager.Add(new Article("Regal"));
+            Lager.Add(new Article("Regal1"));
+            Lager.Add(new Article("Regal2"));
+            Lager.Add(new Article("Regal3"));
+            Lager.Add(new Article("Regal4"));
+            Lager.Add(new Article("Regal5"));
+            Lager.Add(new Electronic("Smartphone1", 1000, 350, 10));
+
+
+            while (true)
+            {
+                Lagerbestand();
+            }
+        }
+
+        static void Lagerbestand()
+        {
+            Console.WriteLine("Lagerbestand:\n");
+
+            for (int i = 0; i < Lager.Count; i++)
+            {
+                Console.WriteLine(Lager[i]);
+            }
+
+            Console.WriteLine($"{Abstandhalter}1) Verkaufen\t2) Wareneingang");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Verkauf();
+                    break;
+                case "2":
+                    Wareneingang();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void Verkauf()
+        {
+            int sellArtNr;
+            int sellCount;
+            string company, buyerName, compAdress, compMail;
+
+            Console.WriteLine("Sell\n");
+
+
+            do
+            {
+                Console.WriteLine("ArtNr: ");
+            } while (!int.TryParse(Console.ReadLine(), out sellArtNr));
+
+            //Does Article Exist?
+
+            if (!Article.Exists(sellArtNr, Lager))
+            {
+                Console.WriteLine("Article does not exist!");
+                return;
+            }
+
+            do
+            {
+                Console.WriteLine("Count: ");
+            } while (!int.TryParse(Console.ReadLine(), out sellCount));
+
+            //Count < available Count?
+
+            if (!Article.CountAvailable(sellArtNr, sellCount, Lager))
+            {
+                Console.WriteLine("Not enough stock!");
+                return;
+            }
+
+            Console.WriteLine($"{Article.getPrize(sellCount, sellArtNr, Lager)}$");
+
+            Console.WriteLine("Contact info:\n");
+
+            Console.WriteLine("Company:");
+            company = Console.ReadLine();
+
+            Console.WriteLine("\nName:");
+            buyerName = Console.ReadLine();
+
+            Console.WriteLine("\nAdress:");
+            compAdress = Console.ReadLine();
+
+            Console.WriteLine("\nE-Mail:");
+            compMail = Console.ReadLine();
+
+            Lager[sellArtNr-1].Count -= sellCount;
+        }
+
+        static void Wareneingang()
+        {
+            int count, price, power;
+            string name;
+
+
+            Console.WriteLine($"{Abstandhalter}1) neuer Artikel\t2) bestehender Artikel");
+
+            if (Convert.ToInt32(Console.ReadLine()) == 1)
+            {
+                Console.WriteLine($"{Abstandhalter}Zu welcher Kategorie gehört der Artikel?\t1) Keiner\t2) Elektronik");
+
+                if (Convert.ToInt32(Console.ReadLine())== 1)
+                {
+                    EingabeProperties_standart(out name, out count, out price);
+
+                    Lager.Add(new Article(name, count, price));
+                }
+                else
+                {
+                    EingabeProperties_electronics(out name, out count, out price, out power);
+
+                    Lager.Add(new Electronic(name, count, price, power));
+                }
+            }
+            else
+            {
+
+            }
+        }
+        
+        //Eingabe von einem normalen Artikel
+
+        static void EingabeProperties_standart(out string name, out int count, out int price)
+        {
+            Console.Write("Name: ");        ////In Methode auslagern
+            name = Console.ReadLine();
+
+            Console.Write("Anzahl: ");
+            int.TryParse(Console.ReadLine(), out count);
+
+            Console.Write("Preis: ");
+            int.TryParse(Console.ReadLine(), out price);
+
+
+        }
+
+        //Eingabe von einem Elektronik-Artikel
+
+        static void EingabeProperties_electronics(out string name, out int count, out int price, out int power)
+        {
+            EingabeProperties_standart(out name, out count, out price);
+
+            Console.Write("Stromverbrauch: ");
+
+            do
+            { } while (int.TryParse(Console.ReadLine(), out power));
+        }
+
+    }
+}
+
